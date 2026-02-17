b8cf9944a59edfc55c379e7b89552aa3ddc9fe64
Pedro Yamato <pedro.yamato@al.infnet.edu.br>
2026-02-17 15:55:59 -0300

    criação das funções de menu de abertura do caixa

--- caixa_supermercado/caixa_supermercado_v1/abertura_caixa.py
+++ caixa_supermercado/caixa_supermercado_v1/abertura_caixa.py
@@ -1,12 +1,24 @@
 from tabulate import tabulate
 
-def abertura_caixa(titulo, op1, op2):
+def menu_abertura_caixa():
     largura = 50
-    opcoes = f"{op1.ljust(largura)}\n{op2.ljust(largura)}"
-    mensagens = [[titulo], [opcoes]]
+    opcoes = f"{'1- Iniciar Atendimento'.ljust(largura)}\n{'2- Fechar Caixa'.ljust(largura)}"
+    mensagens = [["CAIXA DO SUPERMERCADO"], [opcoes]]
     print(tabulate(mensagens, tablefmt="double_grid"))
 
-titulo = "CAIXA DO SUPERMERCADO"
-op1 = "1- Iniciar Atendimento"
-op2 = "2- Fechar Caixa"
-abertura_caixa(titulo, op1, op2)
+
+def entrar_operacao_caixa():
+    menu_abertura_caixa() 
+    while True:
+        opcao = input("Entre com a operação desejada: ")
+        if opcao.isdigit():
+            opcao_caixa = int(opcao)
+            if 1 <= opcao_caixa <= 2:
+                break
+            else:
+                print("Erro: operação inválida!")
+        else:
+            print("Erro: Digite apenas números!")  
+    return opcao_caixa
+
+entrar_operacao_caixa()
