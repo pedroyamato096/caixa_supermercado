ae58acb1d5fa08cbc41df34bcbd251f4e6ecc63e
Pedro Yamato <pedro.yamato@al.infnet.edu.br>
2026-02-23 13:17:03 -0300

    refatoração de mais funções, criação da função de sangria e finalização da primeira versão do projeto

--- caixa_supermercado/caixa_supermercado_v1/caixa_supermercado.py
+++ caixa_supermercado/caixa_supermercado_v1/caixa_supermercado.py
@@ -24,7 +24,7 @@ def ler_dados_produto():
     id_produto = validar_inteiros("Insira o ID do produto (0 para finalizar): ", 0, 5, erro_id_produtos)
     if id_produto == 0:
         return 0, 0
-    qtd_produto = validar_quantidade_produtos("Insira a quantidade do produto:")
+    qtd_produto = validar_quantidade_produtos("Insira a quantidade do produto: ")
     return id_produto, qtd_produto
 
 def buscar_produto(produtos, id_produto):
@@ -40,7 +40,7 @@ def criar_item(id_item, produto, qtd_produto,):
 
 
 def atualizar_estoque(produto, qtd_produto):
-    return produto[2] - qtd_produto
+    produto[2] -= qtd_produto
 
 def emitir_nf(lista):
 
@@ -69,10 +69,6 @@ def iniciar_atendimento(produtos, id_cliente):
     id_cliente +=1 
     return relatorio, id_cliente 
 
-     
-    
-
-
 def criar_lista_itens_cliente(produtos):
     id_item = 1
     itens_cliente = []
@@ -87,9 +83,37 @@ def criar_lista_itens_cliente(produtos):
         else:
             return itens_cliente
         
+def emitir_sangria(relatorio):
+    header = ["Cliente", "Total"]
+    dados = []
+    total_vendas = 0
+    for item in relatorio:
+        cliente = f"Cliente {item[0]}" 
+        total = item[4]
+        dados.append((cliente, total))
+        total_vendas += total
+    print("\nFechamento do caixa")
+    data_hora = retornar_data_hora()
+    print(f"Data: {data_hora}\n")
+    print(tabulate(dados, headers=header))
+    print(f"\nTotal geral: R$ {total_vendas:.2f}")
+
+def produtos_sem_estoque(produtos):
+    sem_estoque = []
+
+    for produto in produtos:
+        if produto[2] <= 0:
+            sem_estoque.append([produto[0], produto[1]])  # ID e Nome
+    if sem_estoque:  # só printa se houver produtos sem estoque
+        print("\nProdutos sem estoque:\n")
+        print(tabulate(sem_estoque, headers=["ID", "Produto"]))
+    else:
+        print("\nNenhum produto sem estoque.\n")
+        
 
 produtos = abrir_caixa()
 id_cliente = 1
+relatorios = []   
 while True:
     opcao_caixa = menu_atendimento()
     if opcao_caixa == 1:
@@ -97,10 +121,13 @@ while True:
         if not relatorio:
             continue
         emitir_nf(relatorio)
+        relatorios.append(relatorio)   
     elif opcao_caixa == 2:
-        print("caixa finalizado")
+        emitir_sangria(relatorios)
+        produtos_sem_estoque(produtos)
+        print("\nCaixa finalizado")
         break
             
 
-# criar a funcao de sangria
+
 
